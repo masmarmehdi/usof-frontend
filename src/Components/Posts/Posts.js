ef8df2541b2d3091a3518ff07c0ee8d4646bfb31
Mehdi Masmar <masmar507@gmail.com>
2021-10-17 20:15:58 +0300

    making posts template

---
+++ src/Components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import Post from "../Post/Post";
+import "./posts.css";
+
+export default function Posts() {
+  return (
+    <div className="posts">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+}
