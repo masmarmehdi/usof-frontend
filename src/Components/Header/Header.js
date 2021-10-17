11596b329342f604fc065a712507208c2d9d503f
Mehdi Masmar <masmar507@gmail.com>
2021-10-17 20:14:13 +0300

    making header template

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header-title">
+        <span>Welcome to MyBlog!</span>
+      </div>
+      <div className="header-cover"></div>
+    </div>
+  );
+}
