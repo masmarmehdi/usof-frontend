12034815080ecf41c0a3e8851b81326accaac988
Mehdi Masmar <masmar507@gmail.com>
2021-10-19 04:06:16 +0300

    Adding routes to the main component(not all of the routes)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import "./App.css";
 import React from "react";
-// import Footer from "./Components/Footer";
 import NavBar from "./Components/NavBar/NavBar";
 import Home from "./Pages/Home/Home";
 import SinglePage from "./Pages/SinglePage/SinglePage";
 import PostCreate from "./Pages/PostCreate/PostCreate";
 import Settings from "./Pages/Settings/Settings";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/register/Register";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
   render() {
+    const user = false;
     return (
-      <div>
+      <Router>
         <NavBar />
-        <Home />
-      </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/settinga">{user ? <Settings /> : <Login />}</Route>
+          <Route path="/login">{user ? <Home /> : <Login />}</Route>
+          <Route path="/register">{user ? <Home /> : <Register />}</Route>
+          <Route path="/posts/:post_id">
+            <SinglePage />
+          </Route>
+          <Route path="/post/create">{user ? <PostCreate /> : <Login />}</Route>
+        </Switch>
+      </Router>
     );
   }
 }
