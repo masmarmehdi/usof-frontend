3de624455fa9e1a4048fec33eab97439b5fd7f97
Mehdi Masmar <masmar507@gmail.com>
2021-10-19 04:10:39 +0300

    adding settings page

--- src/Pages/Settings/Settings.js
+++ src/Pages/Settings/Settings.js
@@ -8,35 +8,52 @@ export default function Settings() {
       <div className="settings-wrapper">
         <div className="settings-title">
           <h1>Settings</h1>
-          <span>Update account</span>
-          <span>Delete account</span>
         </div>
-        <form className="settings-form">
-          <div className="settings-profile-picture">
-            <img className="profile-picture" src={mmasmar} alt="mmasmar" />
-            <span>Role: Admin</span>
-            <span>Rating: 10</span>
+        <div className="settings-card">
+          <div class="settings-account">
+            <span>Update account</span>
+            <button class="settings-delete">Delete account</button>
           </div>
+          <form className="settings-form">
+            <div className="settings-profile-picture">
+              <img className="profile-picture" src={mmasmar} alt="mmasmar" />
+            </div>
 
-          <div className="settings-data">
-            <label>Username</label>
-            <input type="text" value="mmasmar" />
-            <label>Name</label>
-            <input type="text" value="Mehdi Masmar" />
-            <label>email</label>
-            <input type="text" value="masmar507@gmail.com" />
-            <label htmlFor="file_input" className="settings-upload-picture">
-              <span>Upload picture(s)</span>
-              <i class=" add-img-icon fas fa-plus-circle"></i>
-            </label>
-            <input
-              type="file"
-              className="settings-file-input"
-              style={{ display: "none" }}
-            />
-            <button type="submit">Update profile</button>
-          </div>
-        </form>
+            <div className="settings-data">
+              <label className="settings-username">Username</label>
+              <input className="settings-input" type="text" value="mmasmar" />
+              <label className="settings-name">Name</label>
+              <input
+                className="settings-input"
+                type="text"
+                value="Mehdi Masmar"
+              />
+              <label className="settings-email">Email</label>
+              <input
+                className="settings-input"
+                type="text"
+                value="masmar507@gmail.com"
+              />
+              <label className="settings-upload-picture">
+                <span>Upload your profile picture here</span>
+                <i class=" add-img-icon fas fa-plus-circle"></i>
+              </label>
+              <input
+                type="file"
+                className="settings-file-input"
+                style={{ display: "none" }}
+              />
+              <div className="settings-btn">
+                <button type="submit" class="settings-btn-cancel">
+                  Cancel
+                </button>
+                <button type="submit" class="settings-btn-submit">
+                  Update profile
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
       <SideBar />
     </div>
