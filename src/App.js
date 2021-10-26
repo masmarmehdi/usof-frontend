77fde88ad526af93ad4101c0c0d33d0d40fe810a
Mehdi Masmar <masmar507@gmail.com>
2021-10-26 23:57:21 +0300

    adding Pagination component to the home page

--- src/App.js
+++ src/App.js
@@ -7,35 +7,35 @@ import PostCreate from "./Pages/PostCreate/PostCreate";
 import Settings from "./Pages/Settings/Settings";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
+import { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Context } from "./context/Context";
 
-class App extends React.Component {
-  render() {
-    const user = false; // Testing Link component in the condditional statement for now
-    return (
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/settinga">{user ? <Settings /> : <Login />}</Route>
-          <Route path="/login">{user ? <Home /> : <Login />}</Route>
-          <Route path="/register">{user ? <Home /> : <Register />}</Route>
-          <Route path="/posts/:post_id">
-            <SinglePage />
-          </Route>
-          <Route path="/post/create">{user ? <PostCreate /> : <Login />}</Route>
-          <Route path="/users/:username">
-            {user ? <PostCreate /> : <Login />}
-          </Route>
-          <Route path="/categoroes/:category_id/posts">
-            {user ? <PostCreate /> : <Login />}
-          </Route>
-        </Switch>
-      </Router>
-    );
-  }
+function App() {
+  const { user } = useContext(Context);
+
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/settings">{user ? <Settings /> : <Login />}</Route>
+        <Route path="/login">{user ? <Home /> : <Login />}</Route>
+        <Route path="/register">{user ? <Home /> : <Register />}</Route>
+        <Route path="/posts/:post_id">
+          <SinglePage />
+        </Route>
+        <Route path="/post/create">{user ? <PostCreate /> : <Login />}</Route>
+        <Route path="/users/:username">
+          {user ? <PostCreate /> : <Login />}
+        </Route>
+        <Route path="/categoroes/:category_id/posts">
+          {user ? <PostCreate /> : <Login />}
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
-
 export default App;
