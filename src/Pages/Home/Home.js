83835316d174a581823b720c36ab62efeca8f81d
Mehdi Masmar <masmar507@gmail.com>
2021-10-30 23:51:43 +0300

    using Pagination component to paginate the home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,9 +4,11 @@ import SideBar from "../../Components/SideBar/SideBar";
 import "./home.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Pagination from "../../Components/Pagination/Pagination";
 export default function Home() {
   const [posts, setPosts] = useState([]);
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
   useEffect(() => {
     const fetch_posts = async () => {
       const response = await axios.get("http://localhost:8000/api/posts");
@@ -14,13 +16,31 @@ export default function Home() {
     };
     fetch_posts();
   });
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <div>
       <Header />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
       <div className="home">
-        <Posts posts={posts} />
+        <Posts posts={currentPosts} />
+
         <SideBar />
       </div>
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
