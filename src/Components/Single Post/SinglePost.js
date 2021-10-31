4e7d5806fec2ba1c28ce57d307cec1da65abe16d
Mehdi Masmar <masmar507@gmail.com>
2021-11-01 00:51:23 +0200

    adding comments section for a single post

--- src/Components/Single Post/SinglePost.js
+++ src/Components/Single Post/SinglePost.js
@@ -9,7 +9,8 @@ export default function SinglePost() {
   const post_id = location.pathname.split("/")[2];
   const [post, setpost] = useState({});
   const [user, setUser] = useState({});
-
+  const [comments, setComments] = useState([]);
+  let display = false;
   useEffect(() => {
     const fetch_post = async () => {
       const response = await axios.get(
@@ -29,21 +30,45 @@ export default function SinglePost() {
     };
     fetch_user();
   }, [user_id]);
+
+  useEffect(() => {
+    const fetch_post_comments = async () => {
+      const response = await axios.get(
+        `http://localhost:8000/api/posts/${post_id}/comments`
+      );
+      setComments(response.data);
+    };
+    fetch_post_comments();
+  }, [post_id]);
+  const handleComments = () => {
+    if (!display) {
+      document.getElementById("show-comments").style = "display:inherit;";
+      display = true;
+    } else {
+      document.getElementById("show-comments").style = "display:none";
+      display = false;
+    }
+  };
   return (
     <div className="single-post">
+      <h1 style={{ marginLeft: 20 }}>
+        {user.username}'s post about {post.categories}
+      </h1>
       <div className="single-post-wrapper">
         <h1 className="single-post-title">{post.title}</h1>
 
         <div className="single-post-data">
           <span className="post-author">
-            <Link className="link" to="users/:username">
+            <Link className="link" to={"/users/" + post.user_id}>
               <img
                 className="author-img"
                 src={`http://localhost:8000/profile_pictures/${user.profilePicture}`}
                 alt={`${user.username}'s avatar`}
               />
             </Link>
-            <b>{user.username}</b>
+            <Link className="link" to={"/users/" + post.user_id}>
+              <p className="author-username">{user.username}</p>
+            </Link>
           </span>
           <div className="single-post-categories">
             {post.categories
@@ -60,27 +85,67 @@ export default function SinglePost() {
         </div>
         <div className="single-post-images">
           {post.images
-            ? post.images.map((image) =>
-                image !== "" ? (
-                  <img
-                    key={image}
-                    className="single-post-img"
-                    src={`http://localhost:8000/posts_picture/${image}`}
-                    alt={`${user.username}'s post images`}
-                  />
-                ) : null
-              )
+            ? post.images
+                .split("|")
+                .map((image) =>
+                  image !== "" ? (
+                    <img
+                      key={image}
+                      className="single-post-img"
+                      src={`http://localhost:8000/posts_picture/${image}`}
+                      alt={`${user.username}'s post images`}
+                    />
+                  ) : null
+                )
             : null}
         </div>
         <p className="single-post-content">{post.content}</p>
         <div className="activity">
           <div>
-            <i class="far like fa-thumbs-up">{post.likes}</i>
-            <i class="far dislike fa-thumbs-down">{post.dislikes}</i>
+            <i class="fas like fa-thumbs-up ">
+              <span className="count">{post.likes}</span>
+            </i>
+            <i class="fas dislike fa-thumbs-down ">
+              <span className="count">{post.dislikes}</span>
+            </i>
           </div>
-          <div>
-            <i class="far comment fa-comments"></i>
+          <div onClick={handleComments} style={{ cursor: "pointer" }}>
+            <i class="far comment fa-comments">
+              <span className="count">{comments.length}</span>
+            </i>
           </div>
+        </div>
+        <div id="show-comments">
+          {comments.map((comment) => (
+            <div>
+              <div className="comments-section">
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Link className="link" to={"/users/" + post.user_id}>
+                    <img
+                      className="user-reply-img"
+                      src={`http://localhost:8000/profile_pictures/${user.profilePicture}`}
+                      alt=""
+                    />
+                    <p>Username</p>
+                  </Link>
+                  <p>{new Date(comment.created_at).toDateString()}</p>
+                </div>
+                <p key={comment.id} style={{ textAlign: "center" }}>
+                  {comment.content}
+                </p>
+                <div>
+                  <i class="fas comment-like fa-thumbs-up ">
+                    <span className="count">{post.likes}</span>
+                  </i>
+                  <i class="fas comment-dislike fa-thumbs-down ">
+                    <span className="count">{post.dislikes}</span>
+                  </i>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
