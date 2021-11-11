5b9f7806645651b3bca4ea7be0b7fe84ef4be71c
Mehdi Masmar <masmar507@gmail.com>
2021-11-12 01:25:32 +0200

    adding the ability to search for a user, post, category using the search bar. plus some styling

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,25 +1,123 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./navbar.css";
 import myblog from "../../images/MyBlog.png";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { Context } from "../../context/Context";
-
+import axios from "axios";
 export default function NavBar() {
   const { user, dispatch } = useContext(Context);
+
+  const [result, setResult] = useState(null);
+  const [posts, setPosts] = useState(null);
+  const [users, setUsers] = useState(null);
+  const [categories, setCategories] = useState(null);
+
+  useEffect(() => {
+    const fetch_posts = async () => {
+      await axios.get("http://127.0.0.1:8000/api/posts").then((response) => {
+        setPosts(response.data);
+      });
+    };
+    fetch_posts();
+  });
+  useEffect(() => {
+    const fetch_users = async () => {
+      await axios.get("http://127.0.0.1:8000/api/users").then((response) => {
+        setUsers(response.data);
+      });
+    };
+    fetch_users();
+  });
+  useEffect(() => {
+    const fetch_categories = async () => {
+      await axios
+        .get("http://127.0.0.1:8000/api/categories")
+        .then((response) => {
+          setCategories(response.data);
+        });
+    };
+    fetch_categories();
+  });
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
   };
+  function search(value) {
+    if (value.length > 0) {
+      value = value.toUpperCase();
+      const pages = {
+        home: (
+          <Link className="link" to="/">
+            Home
+          </Link>
+        ),
+      };
+      if (posts) {
+        posts.forEach((post) => {
+          pages[post.title] = (
+            <Link
+              className="link"
+              to={`/posts/${post.id}`}
+              onClick={() => (window.location.href = `/posts/${post.id}`)}
+            >
+              {post.title} (post)
+            </Link>
+          );
+        });
+      }
+      if (users) {
+        users.forEach((user) => {
+          pages[user.username] = (
+            <Link
+              className="link"
+              to={`/users/${user.id}`}
+              onClick={() => (window.location.href = `/users/${user.id}`)}
+            >
+              {user.username} (user profile)
+            </Link>
+          );
+        });
+      }
+      if (categories) {
+        categories.forEach((category) => {
+          pages[category.title] = (
+            <Link
+              className="link"
+              to={`/categories/${category.id}`}
+              onClick={() =>
+                (window.location.href = `/categories/${category.id}/posts`)
+              }
+            >
+              {category.title} (category)
+            </Link>
+          );
+        });
+      }
+      setResult(
+        Object.keys(pages).map((key) =>
+          key.toUpperCase().indexOf(value) > -1 ? (
+            <li key={key} className="search-result-list">
+              {pages[key]}
+            </li>
+          ) : null
+        )
+      );
+    } else {
+      setResult(null);
+    }
+  }
   return (
     <div className="nav-bar">
       <div className="navlink-left">
-        <img className="logo-img" src={myblog} alt="" />
+        <Link className="link" to="/">
+          <img className="logo-img" src={myblog} alt="myblog" />
+        </Link>
       </div>
       <div className="navlink-center">
         <ul className="nav-list">
           <li className="nav-link">
             <Link className="link" to="/">
-              Home
+              Posts
             </Link>
           </li>
           <li className="nav-link">
@@ -28,18 +126,55 @@ export default function NavBar() {
             </Link>
           </li>
           <li className="nav-link">
-            <Link className="link" to="/settings">
-              Settings
+            <Link className="link" to="/categories">
+              Categories
             </Link>
           </li>
-          <i className="search-icon fas fa-search"></i>
+          <li className="nav-link">
+            {user && (
+              <Link className="link" to={`/users/${user.id}/posts`}>
+                My posts
+              </Link>
+            )}
+          </li>
+          <li className="nav-link">
+            {user && (
+              <Link className="link" to="/settings">
+                Settings
+              </Link>
+            )}
+          </li>
+          <li className="search-container">
+            <input
+              type="search"
+              className="search-input"
+              onChange={(e) => search(e.target.value)}
+              placeholder="looking for something?"
+            />
+            <div className="search-bar">
+              <ul className="search-result">{result}</ul>
+            </div>
+          </li>
         </ul>
       </div>
 
       {user ? (
         <div className="navlink-right">
-          <p className="user-role">{user.role}</p>
-          <Link className="link" to="/settings">
+          <p className="user-role">
+            {user.role === "admin" ? (
+              <a
+                className="link"
+                href="http://127.0.0.1:8000/admin"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {user.role}
+              </a>
+            ) : (
+              <p>{user.role}</p>
+            )}
+          </p>
+          <Link className="link" to={`/users/${user.id}`}>
             <img
               className="user-img"
               src={`http://127.0.0.1:8000/profile_pictures/${user.profilePicture}`}
