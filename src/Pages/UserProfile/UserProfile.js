86def46e632716d95e90805cf0bb8d1bbbb82680
Mehdi Masmar <masmar507@gmail.com>
2021-11-14 00:20:24 +0200

    adding the ability to see your own posts or a specific user posts + simple style for the link

--- src/Pages/UserProfile/UserProfile.js
+++ src/Pages/UserProfile/UserProfile.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
 import SideBar from "../../Components/SideBar/SideBar";
 import "./userprofile.css";
 
@@ -9,13 +10,9 @@ export default function Settings() {
   const user_id = location.pathname.split("/")[2];
   const [user, setUser] = useState({});
   useEffect(() => {
-    const fetch_post = async () => {
-      const response = await axios.get(
-        `http://localhost:8000/api/users/${user_id}`
-      );
+    axios.get(`http://localhost:8000/api/users/${user_id}`).then((response) => {
       setUser(response.data);
-    };
-    fetch_post();
+    });
   }, [user_id]);
   return (
     <div className="user-profile">
@@ -24,6 +21,9 @@ export default function Settings() {
           <h1>{user.username}'s Profile</h1>
         </div>
         <div className="user-profile-card">
+          <Link className="link users-post-link" to={`/users/${user_id}/posts`}>
+            {user.username} 's posts
+          </Link>
           <div className="user-profile-profile-picture">
             <img
               className="profile-picture"
@@ -31,6 +31,7 @@ export default function Settings() {
               alt={`${user.username}'s Avatar`}
             />
           </div>
+
           <div>
             <p className="user-profile-data">Username: {user.username}</p>
             <p className="user-profile-data">Name: {user.name}</p>
