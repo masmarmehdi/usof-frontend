e955ddbd5f7267cb99e4417557d07e972a0beffe
Mehdi Masmar <masmar507@gmail.com>
2021-11-23 02:23:44 +0200

    adding the ability to edit or delete a comment

--- src/Components/Comment/Comment.js
+++ src/Components/Comment/Comment.js
@@ -1,51 +1,122 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import LikeDislike from "../LikeDislike/LikeDislike";
-
-export default function Comment({ comment, post }) {
-  const [user, setUser] = useState({});
-  const user_id = comment.user_id;
-  // console.log(comment);
+import { Context } from "../../context/Context";
+import CreateCommentLikeDislike from "../CreateCommentLikeDislike/CreateCommentLikeDislike";
+import "./comment.css";
+export default function Comment({ post_id, comment }) {
+  const { user } = useContext(Context);
+  const [commentedUser, setCommentedUser] = useState({});
+  const [content, setContent] = useState("");
+  const [updateMode, setupdateMode] = useState(false);
+  const commentedUser_id = comment.user_id;
   useEffect(() => {
     const fetch_user = async () => {
       const response = await axios.get(
-        `http://localhost:8000/api/users/${user_id}`
+        `http://localhost:8000/api/users/${commentedUser_id}`
       );
-      setUser(response.data);
+      setCommentedUser(response.data);
     };
     fetch_user();
-  }, [user_id]);
+  }, [commentedUser_id]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let data = new FormData();
+    data.append("user_id", user.id);
+    data.append("_method", "PATCH");
+    data.append("content", content);
+
+    console.log(data);
+    const response = await axios.post(
+      `http://127.0.0.1:8000/api/comments/${comment.id}`,
+      data
+    );
+    console.log(response.data);
+    if (response.data) {
+      setupdateMode(false);
+      window.location.reload();
+    }
+  };
+  const updateComment = () => {
+    if (updateMode) {
+      setupdateMode(false);
+    } else {
+      setupdateMode(true);
+    }
+  };
+  const handleCommentChange = (e) => {
+    setContent(e.target.value);
+  };
+  const deleteComment = async () => {
+    const response = await axios.delete(
+      `http://127.0.0.1:8000/api/comments/${comment.id}`,
+      {
+        user_id: user.id,
+      }
+    );
+    if (!response.data.error) {
+      window.location.reload();
+    }
+  };
   return (
     <div>
       <div>
         <div className="comments-section">
           <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <Link className="link" to={"/users/" + comment.user_id}>
+            <Link className="link" to={"/users/" + commentedUser_id}>
               <img
                 className="user-reply-img"
-                src={`http://localhost:8000/profile_pictures/${user.profilePicture}`}
+                src={`http://localhost:8000/profile_pictures/${commentedUser.profilePicture}`}
                 alt=""
               />
-              <p>{user.username}</p>
+              <p>{commentedUser.username}</p>
             </Link>
             <p>{new Date(comment.created_at).toDateString()}</p>
+            {commentedUser_id === user.id ? (
+              <div className="update-delete-comment">
+                <div>
+                  <button className="edit-comment-btn" onClick={updateComment}>
+                    Edit
+                  </button>
+                </div>
+                <div>
+                  <button
+                    onClick={deleteComment}
+                    className="delete-comment-btn"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            ) : null}
           </div>
-          <p key={comment.id} style={{ textAlign: "center" }}>
-            {comment.content}
-          </p>
-          <div>
-            {/* <LikeDislike type="dislike" comment_id={comment.id} /> */}
+          {updateMode ? (
+            <div>
+              <form onSubmit={handleSubmit} className="comment-form">
+                <input
+                  type="text"
+                  className="update-comment-form"
+                  placeholder={comment.content}
+                  onChange={handleCommentChange}
+                />
+                <button className="save-comment-btn">Save changes</button>
+              </form>
+            </div>
+          ) : (
+            <p
+              key={comment.id}
+              style={{ textAlign: "center" }}
+              className="comment-content"
+            >
+              {comment.content}
+            </p>
+          )}
 
-            <i class="fas comment-like fa-thumbs-up ">
-              <span className="count">{comment.likes}</span>
-            </i>
-            {/* <LikeDislike type="dislike" comment_id={comment.id} /> */}
-
-            <i class="fas comment-dislike fa-thumbs-down ">
-              <span className="count">{comment.dislikes}</span>
-            </i>
-          </div>
+          <CreateCommentLikeDislike
+            comment_id={comment.id}
+            comment={comment}
+            post_id={post_id}
+          />
         </div>
       </div>
     </div>
